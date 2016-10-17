00ca154979cd21131a948c2903f2d0c70b13d30e
Angus Thomsen <aksthomsen@gmail.com>
2016-10-18 00:23:52 +1100

    documents average-sizes

--- sql/queries/average-sizes.sql
+++ sql/queries/average-sizes.sql
@@ -1,26 +1,28 @@
 WITH
 
-time_avg AS (
-  SELECT
-    package.name AS name,
-    result.version AS ghc_version,
-    avg(result.seconds) AS average_time
-  FROM package, batch, result
-  WHERE package.id = batch.package
-    AND batch.id   = result.batch
-  GROUP BY name, ghc_version),
-
-file_size AS (
-  SELECT
-    package.name AS name,
-    result.version AS ghc_version,
-    sum(file_output.file_size) AS total_size
-  FROM package, batch, result, file_output
-  WHERE package.id = batch.package
-    AND batch.id   = result.batch
-    AND result.id  = file_output.result
-    AND file_output.file_extension = 'hi'
-  GROUP BY name, ghc_version)
+  -- aggregate the average compilation times
+  time_avg AS (
+    SELECT
+      package.name AS name,
+      result.version AS ghc_version,
+      avg(result.seconds) AS average_time
+    FROM package, batch, result
+    WHERE package.id = batch.package
+      AND batch.id   = result.batch
+    GROUP BY name, ghc_version),
+  
+  -- aggregate the total size of hi file type
+  file_size AS (
+    SELECT
+      package.name AS name,
+      result.version AS ghc_version,
+      sum(file_output.file_size) AS total_size
+    FROM package, batch, result, file_output
+    WHERE package.id = batch.package
+      AND batch.id   = result.batch
+      AND result.id  = file_output.result
+      AND file_output.file_extension = 'hi'
+    GROUP BY name, ghc_version)
 
 SELECT time_avg.name,
        time_avg.ghc_version,
